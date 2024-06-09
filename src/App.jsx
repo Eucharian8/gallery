a1069e6b296367d032c738aa643050218aa36d30
Eucharia <eucharian8@gmail.com>
2024-06-09 23:59:20 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Gallery from "./components/gallary";
+
+function App() {
+  return (
+    <>
+      <Gallery />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/dummy.jsx
@@ -0,0 +1,27 @@
+export const images = [
+    {
+      id: 1,
+      name: "Black Cat",
+      url: "https://media.istockphoto.com/id/1361394182/photo/funny-british-shorthair-cat-portrait-looking-shocked-or-surprised.jpg?s=1024x1024&w=is&k=20&c=-keJ_V9tZtTsxwcuJqrS9Zx0EJNglQU1ezZrYYIBYrQ=",
+    },
+    {
+      id: 2,
+      name: "white Cat",
+      url: "https://media.istockphoto.com/id/104355461/photo/front-view-of-british-shorthair-cat-7-months-old-sitting.jpg?s=1024x1024&w=is&k=20&c=bCr4FDuiEBSaQvU6xpi8JH2NPFPli2cY1ZVKQvPnEcc=",
+    },
+    {
+      id: 3,
+      name: "White Dog",
+      url: "https://media.istockphoto.com/id/1482199015/photo/happy-puppy-welsh-corgi-14-weeks-old-dog-winking-panting-and-sitting-isolated-on-white.jpg?s=1024x1024&w=is&k=20&c=XCKHaoM9oG4ST-sLawqYyutywWXkx3DYWb4MKhLUBrI=",
+    },
+    {
+      id: 4,
+      name: "Black Dog",
+      url: "https://media.istockphoto.com/id/1293292142/photo/portrait-of-a-labrador-retriever-dog-on-an-isolated-black-background.jpg?s=1024x1024&w=is&k=20&c=Vt8wqJpb5ALmTom50dIpPxj-g0Tas1kYrI4ZxgLIB5g=",
+    },
+    {
+      id: 5,
+      name: "Chicken",
+      url: "https://media.istockphoto.com/id/1217649450/photo/chicken-or-hen-on-a-green-meadow.jpg?s=1024x1024&w=is&k=20&c=UirOEsuUZmqsCDaVL97EvgzEzd7WGjKAXw2pMpr6VvA=",
+    },
+  ];
